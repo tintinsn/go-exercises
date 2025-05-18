24f4122c3602fb71ee748c5e6e49cec41a4ea0ee
tintin <tin.sn98@gmail.com>
2025-05-18 22:10:47 +0700

    add exercise 1 - print number

--- loops/for_loop_exercises.go
+++ loops/for_loop_exercises.go
@@ -1,7 +1,17 @@
 package main
 
+import "fmt"
+
+// * Beginner
+
+// Exercise 1: Print numbers
+func printNumber(num int) {
+	for i:=1; i<= num; i++ {
+		fmt.Println(i)
+	}
+}
 
 func main() {
-
+	printNumber(10)
 
 }
