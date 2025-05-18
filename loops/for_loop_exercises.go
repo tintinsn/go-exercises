4657f8bbf4deceff1c6e2ae65b485fcf04c14474
tintin <tin.sn98@gmail.com>
2025-05-18 22:20:52 +0700

    add exercise 2 - even number

--- loops/for_loop_exercises.go
+++ loops/for_loop_exercises.go
@@ -5,13 +5,23 @@ import "fmt"
 // * Beginner
 
 // Exercise 1: Print numbers
-func printNumber(num int) {
-	for i:=1; i<= num; i++ {
-		fmt.Println(i)
-	}
+// func printNumber(num int) {
+// 	for i:=1; i<= num; i++ {
+// 		fmt.Println(i)
+// 	}
+// }
+
+// Exercise 2: Print Even number
+func evenNumber(num int) {
+	for i:=0; i<= num; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	} 
 }
 
-func main() {
-	printNumber(10)
 
+
+func main() {
+	evenNumber(15)
 }
